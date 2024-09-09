ff9cb8abfda520234610c916341829a7cc468487
retroswan <world5ziggurat@gmail.com>
2024-09-08 23:21:31 -0400

    Defined API better

--- src/tinydraw/tinydraw.h
+++ src/tinydraw/tinydraw.h
@@ -8,6 +8,9 @@
 
 int TinyDraw_Init(void);
 
+// Takes a width & height
+void TinyDraw_Create_RenderTarget(void);
+
 SDL_GPUShader* TinyDraw_Load_Shader(
     const char* fileName,
     Uint32 samplerCount,
@@ -17,14 +20,28 @@ SDL_GPUShader* TinyDraw_Load_Shader(
     SDL_GPUShaderStage stage
 );
 
+// Takes a string
 void TinyDraw_Load_Texture(void);
 
-void TinyDraw_Draw_Sprite(void);
+// Takes shader
+// Frag or vertex & will do the right thing either way
+void TinyDraw_Bind_Shader(void);
 
-void TinyDraw_Present(void);
+// Takes texture
+// Run for each `TinyDraw_Render`
+void TinyDraw_Bind_Texture(void);
 
-void TinyDraw_Unload_Shader(void);
+// Takes float2 dest pos, float2 dest size, int2 source pos, int2 source size
+void TinyDraw_Stage_Sprite(void);
 
+// Takes rendertarget (or null to send to window)
+// Uploads vertices to GPU & draws to render target
+void TinyDraw_Render(void);
+
+// Sends to window & ends the frame
+void TinyDraw_EndFrame(void);
+
+// Takes texture
 void TinyDraw_Unload_Texture(void);
 
 void TinyDraw_Quit(void);
